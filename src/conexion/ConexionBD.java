e6da56876838a016fdf343d3e5a43102c3e34bec
am-lozano <am.lozano@iesnervion.es>
2025-05-13 14:40:34 +0200

    terminar y corregir ej 1

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -6,15 +6,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import conexion.boletin1.constantes.Constantes;
+
 public class ConexionBD {
 
 	public static void main(String[] args) {
 		
-		String url = "jdbc:mysql://localhost/institutodb";
-		String usuario = "root";
-		String pas = "RootUser132.";
+//		String url = "jdbc:mysql://localhost/institutodb";
+//		String usuario = "root";
+//		String pas = "RootUser132.";
 		
-		try(Connection con = DriverManager.getConnection(url, usuario, pas)) {
+		try(Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.PASSWORD)) {
 			
 			System.out.println("La conexion ha sido un exito");
 			
@@ -24,7 +26,7 @@ public class ConexionBD {
 			
 			while (rs.next()) {
 				
-				
+				System.out.println(rs.getString(1) + " " + rs.getString(2));
 				
 			}
 			
